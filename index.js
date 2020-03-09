fba22523f57d6e16edb92a14ae58e98b1acf6cad
Andrew Hinger <andrewjhinger@gmail.com>
2020-03-09 21:07:48 +0100

    animate timer

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hi there");
+const durationInput = document.querySelector("#duration");
+const pauseButton = document.querySelector("#pause");
+const startButton = document.querySelector("#start");
+const circle = document.querySelector("circle");
+
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("timer started");
+  },
+  onTick() {
+    const steps = perimeter / durationInput.value;
+    circle.setAttribute("stroke-dashoffset", currentOffset);
+    currentOffset = currentOffset - 1;
+    console.log("timer just ticked down");
+  },
+  onComplete() {
+    console.log("timer complete");
+  }
+});
+
+timer.start();
---
+++ timer.js
@@ -0,0 +1,45 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+    //this in
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick();
+    this.interval = setInterval(this.tick, 50);
+  };
+  tick = () => {
+    // const timeRemaining = this.timeRemaining; hidden away the complexity of how we are getting and setting value
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) this.onComplete();
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.5;
+      if (this.onTick) this.onTick();
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+  pause = () => {
+    clearInterval(this.interval);
+    console.log("pause button");
+  };
+}
